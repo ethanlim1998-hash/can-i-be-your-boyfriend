c9b0906ef53dfceab37182a6db11cd419a7963ee
ethanlim1998-hash <ethanlim1998@gmail.com>
2026-01-04 02:08:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! 🥹";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
